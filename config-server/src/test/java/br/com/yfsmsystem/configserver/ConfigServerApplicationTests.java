3b66d6e7b8bb6eeab37d4a35909be8ff57550178
yallisonmelo <yallisonmelo@gmail.com>
2021-05-08 17:05:38 -0300

    implements config-server and configure product-central profiles

---
+++ config-server/src/test/java/br/com/yfsmsystem/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.yfsmsystem.configserver;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
